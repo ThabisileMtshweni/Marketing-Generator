5e1c893f3f2c591c071298daf5a338fae032c260
banelexhamlashe345 <banelebanele938@gmail.com>
2025-06-11 22:34:49 +0200

    Add build.sh to install PortAudio on Render

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y portaudio19-dev
+# System dependencies
+apt-get update && apt-get install -y \
+    python3-dev \
+    gcc \
+    libportaudio2 \
+    portaudio19-dev \
+    libasound2-dev
 
-pip install --upgrade pip
+# Python environment
+python -m pip install --upgrade pip
+pip install wheel setuptools
+
+# Install requirements
 pip install -r requirements.txt
